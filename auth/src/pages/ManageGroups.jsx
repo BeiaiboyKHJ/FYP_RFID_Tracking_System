f5e0456fb824936cb435174b329d4864c0901abc
Jin <hengjinkoey@gmail.com>
2026-06-25 18:15:45 +0800

    Manage group updated 25/6

--- auth/src/pages/ManageGroups.jsx
+++ auth/src/pages/ManageGroups.jsx
@@ -469,7 +469,6 @@ const ManageGroups = ({ role }) => {
     <div className="p-8 bg-[#FBFBFE] min-h-screen font-sans">
       <div className="max-w-7xl mx-auto">
         <div className="mb-10 flex justify-between items-start">
-
           <div>
             <h1 className="text-4xl font-black text-slate-900 tracking-tight mb-2">Group Management</h1>
             <div className="flex items-center gap-2">
@@ -485,9 +484,6 @@ const ManageGroups = ({ role }) => {
               <Plus size={20} /> Create Team
             </button>
           )}
-        </div>
-        <div className="mb-16">
-          <GroupTimeStatus />
         </div>
 
         {loading ? (
